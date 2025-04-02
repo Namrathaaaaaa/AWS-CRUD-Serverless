d3e067113fa9281dae3885a6d5b3f0786fd7a919
Rishi Kumar Tiwari <rishi008.tiwari@gmail.com>
2025-04-02 19:24:11 +0530

    fix - create zip for mjs

--- Layers/create_zip.sh
+++ Layers/create_zip.sh
@@ -3,25 +3,25 @@ zip -r layer.zip nodejs
 
 echo "Creating zip for GET Function"
 cd LambdaFunctionsWithLayer/get
-zip -r get.zip index.js
+zip -r get.zip index.mjs
 mv get.zip ../../
 cd ../..
 
 echo "Creating zip for POST Function"
 cd LambdaFunctionsWithLayer/post
-zip -r post.zip index.js
+zip -r post.zip index.mjs
 mv post.zip ../../
 cd ../..
 
 echo "Creating zip for UPDATE Function"
 cd LambdaFunctionsWithLayer/update
-zip -r update.zip index.js
+zip -r update.zip index.mjs
 mv update.zip ../../
 cd ../..
 
 echo "Creating zip for DELETE Function"
 cd LambdaFunctionsWithLayer/delete
-zip -r delete.zip index.js
+zip -r delete.zip index.mjs
 mv delete.zip ../../
 cd ../..
 echo "Success!"
